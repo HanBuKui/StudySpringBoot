1316243c3342e835e111e7b75a2a1e156d6e090c
HanBuKui <1040069852@qq.com>
2022-12-26 23:33:12 +0800

    【demo项目-异常消息统一 RestControllerAdvice】

---
+++ springboot_ssmp/src/main/java/com/zjh/controller/utils/ProjectExceptionAdvice.java
@@ -0,0 +1,29 @@
+package com.zjh.controller.utils;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @ClassName: ProjectExceptionAdvice
+ * @Description: 作为springmvc的异常处理器
+ * @author: zjh
+ * @date: 2022/12/26  23:20
+ * @Version: 1.0
+ */
+
+/*
+
+ */
+@RestControllerAdvice
+public class ProjectExceptionAdvice {
+
+    //拦截所有的异常信息
+    @ExceptionHandler  //处理什么异常由此决定
+    public Result doException(Exception ex){
+        //记录日志
+        //通知运维
+        //通知开发
+        ex.printStackTrace();  //控制台的信息要显示！
+        return new Result(ex.getMessage());
+    }
+}
--- springboot_ssmp/src/main/java/com/zjh/controller/utils/Result.java
+++ springboot_ssmp/src/main/java/com/zjh/controller/utils/Result.java
@@ -19,9 +19,20 @@ import lombok.NoArgsConstructor;
 public class Result {
     private Boolean flag;
     private Object data;
-
+    private String msg;
     public Result(boolean flag){
         this.flag = flag;
     }
-
+    public Result(boolean flag,String msg){
+        this.flag = flag;
+        this.msg = msg;
+    }
+    public Result(boolean flag,Object data){
+        this.flag = flag;
+        this.data = data;
+    }
+    public Result(String msg){
+        this.flag = false;
+        this.data = msg;
+    }
 }

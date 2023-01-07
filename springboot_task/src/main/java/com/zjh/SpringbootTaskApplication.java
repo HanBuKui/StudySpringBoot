1cab7bccc340d1b26bbc28cbc870852092378a2c
HanBuKui <1040069852@qq.com>
2023-01-07 23:41:49 +0800

    【springboot-定时任务学习】

---
+++ springboot_task/src/main/java/com/zjh/SpringbootTaskApplication.java
@@ -0,0 +1,13 @@
+package com.zjh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTaskApplication.class, args);
+	}
+
+}

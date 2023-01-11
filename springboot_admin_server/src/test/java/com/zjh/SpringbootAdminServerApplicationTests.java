06b3cacb2202bcd9b60b3059142086520d53831b
HanBuKui <1040069852@qq.com>
2023-01-11 21:07:38 +0800

    【springboot-监控-SpringBootAdmin】

---
+++ springboot_admin_server/src/test/java/com/zjh/SpringbootAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
